d579cde286ceb17c05f7918bcd7d9a9e4883f3a2
glassnekeep <56785261+glassnekeep@users.noreply.github.com>
2022-05-02 23:52:45 +0300

    Added current program fragment, onClicks for items in recyclerview adapter and passing program id from programs fragment to program fragment

--- app/src/main/java/application/workout/fitnessmobileproject/views/adapters/ProgramAdapter.kt
+++ app/src/main/java/application/workout/fitnessmobileproject/views/adapters/ProgramAdapter.kt
@@ -3,15 +3,20 @@ package application.workout.fitnessmobileproject.views.adapters
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import application.workout.fitnessmobileproject.R
 import application.workout.fitnessmobileproject.databinding.ItemProgramBinding
 import application.workout.fitnessmobileproject.model.models.Program
+import application.workout.fitnessmobileproject.views.fragments.HomeViewPagerFragmentDirections
+import application.workout.fitnessmobileproject.views.fragments.ProgramsFragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
-class ProgramAdapter(val context: Context, val programList: List<Program>): RecyclerView.Adapter<ProgramAdapter.ProgramViewHolder>() {
+class ProgramAdapter(
+    val context: Context,
+    val programList: List<Program>): RecyclerView.Adapter<ProgramAdapter.ProgramViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProgramViewHolder {
         val binding = ItemProgramBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -32,8 +37,17 @@ class ProgramAdapter(val context: Context, val programList: List<Program>): Recy
                     //.override(331, 136)
                     .into(binding.imageProgram)
                 //Glide.with(binding.root.context).load(this.image).into(binding.imageProgram)
+                binding.root.setOnClickListener {
+                    val action = HomeViewPagerFragmentDirections.actionHomeViewPagerFragmentToProgramFragment(this.id)
+                    it.findNavController().navigate(action)
+                }
             }
+            /*itemView.setOnClickListener {
+                val action = HomeViewPagerFragmentDirections.actionHomeViewPagerFragmentToProgramFragment()
+                it.findNavController().navigate(R.id.action_homeViewPagerFragment_to_programFragment)
+            }*/
         }
+        holder.binding
     }
 
     override fun getItemCount(): Int {
