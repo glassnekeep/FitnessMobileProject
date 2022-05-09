ea5a612e79963d0897451487158fe89a89f5926c
glassnekeep <56785261+glassnekeep@users.noreply.github.com>
2022-05-10 02:54:46 +0300

    Created ExerciseAdapter and added id to an image in exercise item

---
+++ app/src/main/java/application/workout/fitnessmobileproject/views/adapters/ExerciseAdapter.kt
@@ -0,0 +1,42 @@
+package application.workout.fitnessmobileproject.views.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import application.workout.fitnessmobileproject.R
+import application.workout.fitnessmobileproject.databinding.ItemExerciseBinding
+import application.workout.fitnessmobileproject.model.models.Exercise
+import com.bumptech.glide.Glide
+
+class ExerciseAdapter(
+    val context: Context,
+    val exerciseList: List<Exercise>): RecyclerView.Adapter<ExerciseAdapter.ExerciseViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseViewHolder {
+        val binding = ItemExerciseBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ExerciseViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ExerciseViewHolder, position: Int) {
+        with(holder) {
+            with(exerciseList[position]) {
+                binding.exerciseName.text = this.name
+                binding.numberOfApproaches.text = this.numberOfApproaches.toString()
+                Glide
+                    .with(binding.root.context)
+                    .load(this.image)
+                    .placeholder(R.drawable.place_holder)
+                    .centerCrop()
+                    .into(binding.exerciseImage)
+            }
+        }
+        holder.binding
+    }
+
+    override fun getItemCount(): Int {
+        return exerciseList.size
+    }
+
+    inner class ExerciseViewHolder(val binding: ItemExerciseBinding): RecyclerView.ViewHolder(binding.root)
+}
