27b2b7507049afc8e38e495cd4581becd6d783f3
glassnekeep <56785261+glassnekeep@users.noreply.github.com>
2022-05-22 00:07:24 +0300

    Profile layout and menu created

---
+++ app/src/main/java/application/workout/fitnessmobileproject/views/fragments/ProfileFragment.kt
@@ -0,0 +1,24 @@
+package application.workout.fitnessmobileproject.views.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import application.workout.fitnessmobileproject.R
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+}
